aea968e79e3474fc5b403d22d292fabc60cce67c
gato001 <102332233+gato001k1@users.noreply.github.com>
2023-01-22 14:25:33 -0500

    Update chatrecieversftp.bat

--- chatrecieversftp.bat
+++ chatrecieversftp.bat
@@ -9,17 +9,17 @@ echo %random%%random%%random%%random% > numdif.lf )
 
 :inic
 echo Ingrese la dirección IP o el nombre de host del servidor SFTP:
-set /p sftpServer=
+set /p ftpServer=
 
 echo Ingrese el nombre de usuario para iniciar sesión en el servidor SFTP:
-set /p sftpUsername=
+set /p ftpUsername=
 
 echo Ingrese la contraseña para iniciar sesión en el servidor SFTP:
-set /p sftpPassword=
+set /p ftpPassword=
 
 :loop
-curl -u %sftpUsername%:%sftpPassword% sftp://%sftpServer% -Q "get message.txt path/to/remote/file/test1"
-curl -u %sftpUsername%:%sftpPassword% sftp://%sftpServer% -Q "get crlm.rl path/to/remote/file/test1"
+curl -u %sftpUsername%:%sftpPassword% ftp://%ftpServer% -Q "get message.txt path/to/remote/file/test1"
+curl -u %sftpUsername%:%sftpPassword% ftp://%ftpServer% -Q "get crlm.rl path/to/remote/file/test1"
 echo Ingrese el mensaje a enviar o escribe <> para recargar
 set /p message=
 :: this is to confirm if there is any new message
@@ -30,7 +30,7 @@ echo %message% >> message1.txt
 ::this is to diplay the incoming messages
 type message.txt
 
-curl -u %sftpUsername%:%sftpPassword% sftp://%sftpServer% -Q "put message%numdifnum%.txt path/to/remote/file/test1"
-curl -u %sftpUsername%:%sftpPassword% sftp://%sftpServer% -Q "put crlm%numdifnum%.rl path/to/remote/file/test1"
+curl -u %sftpUsername%:%ftpPassword% ftp://%ftpServer% -Q "put message%numdifnum%.txt path/to/remote/file/test1"
+curl -u %sftpUsername%:%ftpPassword% ftp://%ftpServer% -Q "put crlm%numdifnum%.rl path/to/remote/file/test1"
 
 goto loop
