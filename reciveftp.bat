5a52d551e5449587afb2515749ab4670c4503955
gato001 <102332233+gato001k1@users.noreply.github.com>
2023-07-06 17:49:21 -0400

    Update reciveftp.bat

--- reciveftp.bat
+++ reciveftp.bat
@@ -1,4 +1,5 @@
 @echo off
+title RECIVER
 
 :inic
 cd users
@@ -7,11 +8,14 @@ cd users
 echo catching the user
 if not exist reciverin.l (
     echo waiting for reciverin.l
+    echo waiting for reciver_filename.l
     timeout /t 1 > nul
     goto o
 ) else (
     set /p user=<reciverin.l
+    set /p filename_save=<reciver_filename.l
     del reciverin.l
+    del reciver_filename.l
     goto sender
 )
 
@@ -22,16 +26,18 @@ cd %user%
 set /p username=<username
 set /p password=<password
 set /p server=<server
+echo %filename_save%
 echo %username%
 echo %password%
 echo %server%
 
 :loop
 echo Checking file...
-curl --user %username%:%password% --head --silent ftp://%server%/test.txt > header.txt
+timeout /t 1 > nul
+curl --user %username%:%password% --head --silent ftp://%server%/%filename_save% > header.txt
 for /F "tokens=2 delims=: " %%i in ('type header.txt ^| findstr /i "Content-Length:"') do set server_file_size=%%i
-if exist test.txt (
-    for %%A in (test.txt) do set local_file_size=%%~zA
+if exist %filename_save% (
+    for %%A in (%filename_save%) do set local_file_size=%%~zA
 ) else (
     echo The message file does not exist.
     set local_file_size=0
@@ -44,7 +50,7 @@ if %local_file_size% equ %server_file_size% (
     echo File is the same, no changes made.
 ) else (
     echo File has changed. Downloading file...
-    curl --user %username%:%password% -o test.txt ftp://%server%/test.txt
+    curl --user %username%:%password% -o %filename_save% ftp://%server%/%filename_save%
     echo refreshing text screen
     echo 0>text.refresh
 )
